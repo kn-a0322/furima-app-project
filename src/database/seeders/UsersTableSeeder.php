e29a4a8a2afa07a5a03429ef93b232d8e18fa271
kana <kana.umeda.03220409@gmail.com>
2026-03-29 04:15:37 +0900

    improve:UsersTableSeeder

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -30,5 +30,24 @@ class UsersTableSeeder extends Seeder
             ],
         ];
         DB::table('users')->insert($users);
+
+        DB::table('profiles')->insert([
+            [
+                'user_id' => 1, // 山田太郎
+                'postcode' => '123-4567',
+                'address' => '神奈川県相模原市南区',
+                'building' => 'ハイツ101',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 2, // 山田花子
+                'postcode' => '987-6543',
+                'address' => '神奈川県相模原市中央区',
+                'building' => 'ハイツ202',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
